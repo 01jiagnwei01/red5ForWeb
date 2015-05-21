f782a258c218770deaf6c3b6b93c260fe85d4d40
xubaoyong <xubaoyong@paypalm.cn>
2015-05-21 12:59:38 +0800

    red5initListener

--- src/main/java/com/gxkj/red5/listeners/Red5InitListener.java
+++ src/main/java/com/gxkj/red5/listeners/Red5InitListener.java
@@ -2,18 +2,32 @@ package com.gxkj.red5.listeners;
 
 import java.io.File;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class Red5InitListener extends ContextLoaderListener  {
  
 	protected final  Logger logger = LoggerFactory.getLogger(getClass());
 
+	/**
+	 * spring上下文环境  
+	 */
+	private static WebApplicationContext applicationContext;
+
+	/**
+	 * servlet上下文环境
+	 */
+	private static ServletContext servletContext;
+
+	
  
-	public void contextInitialized(ServletContextEvent sce) {
+	public void contextInitialized(ServletContextEvent event) {
 		File dir = new File(this.getClass().getResource("").getPath()); 
 		String dirAbsoultePath = dir.getAbsolutePath();
 		String classes = "classes";
@@ -21,7 +35,15 @@ public class Red5InitListener extends ContextLoaderListener  {
 		 
 		 System.setProperty("red5.root",classpath);
 		 System.setProperty("red5.config_root",String.format("%s/%s", classpath,"conf"));
-		 System.out.println(System.getProperty("red5.config_root"));
+		 
+		 logger.info("set red5.root:{}",classpath);
+		 logger.info("set red5.config_root:{}", System.getProperty("red5.config_root"));
+	
+		 
+		 super.contextInitialized(event);
+		servletContext = event.getServletContext();
+		applicationContext = WebApplicationContextUtils
+				.getRequiredWebApplicationContext(servletContext);
 		
 	}
  
@@ -29,7 +51,49 @@ public class Red5InitListener extends ContextLoaderListener  {
 	 
 		
 	}   
+	/**
+	 * @return the applicationContext
+	 */
+	public static WebApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
 
+	/**
+	 * @param applicationContext
+	 *            the applicationContext to set
+	 */
+	public static void setApplicationContext(WebApplicationContext applicationContext) {
+		Red5InitListener.applicationContext = applicationContext;
+	}
+
+	/**
+	 * @return the servletContext
+	 */
+	public static ServletContext getServletContext() {
+		return servletContext;
+	}
+
+	/**
+	 * @param servletContext
+	 *            the servletContext to set
+	 */
+	public static void setServletContext(ServletContext servletContext) {
+		Red5InitListener.servletContext = servletContext;
+	}
+
+	public static Object getBean(String beanName) {
+		 
+		if(applicationContext==null){
+			 System.out.println("异常：applicationContext为空");
+			return null;
+		}
+		if(!applicationContext.containsBean(beanName)){
+			 System.out.println("异常：applicationContext中没有beanName="+beanName);
+				return null;
+		}
+		 
+		return applicationContext.getBean(beanName);
+	}
 	
 	 
 }
