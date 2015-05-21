42e580e94d58901d922e3d376b48e0ecc4a94863
xubaoyong <xubaoyong@paypalm.cn>
2015-05-21 12:49:09 +0800

    init red5

--- src/main/java/com/gxkj/red5/listeners/Red5InitListener.java
+++ src/main/java/com/gxkj/red5/listeners/Red5InitListener.java
@@ -3,18 +3,13 @@ package com.gxkj.red5.listeners;
 import java.io.File;
 
 import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.http.HttpServlet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.context.ContextLoaderListener;
 
-public class Red5InitListener extends HttpServlet implements ServletContextListener{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2227351068264248685L;
+public class Red5InitListener extends ContextLoaderListener  {
+ 
 	protected final  Logger logger = LoggerFactory.getLogger(getClass());
 
  
@@ -23,14 +18,15 @@ public class Red5InitListener extends HttpServlet implements ServletContextListe
 		String dirAbsoultePath = dir.getAbsolutePath();
 		String classes = "classes";
 		String classpath = dirAbsoultePath.substring(0, dirAbsoultePath.indexOf(classes)+classes.length());
-		 System.out.println(dirAbsoultePath);
-		 System.out.println(classpath);
+		 
 		 System.setProperty("red5.root",classpath);
+		 System.setProperty("red5.config_root",String.format("%s/%s", classpath,"conf"));
+		 System.out.println(System.getProperty("red5.config_root"));
 		
 	}
  
 	public void contextDestroyed(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
+	 
 		
 	}   
 
